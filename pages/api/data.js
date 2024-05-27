04396ba2b6910cfc860e972757c5257aa82a9b9b
Amélie Rolland <amelierolland.v@gmail.com>
2024-05-27 12:18:47 +0200

    cleaning

--- pages/api/data.js
+++ pages/api/data.js
@@ -20,11 +20,4 @@ export default async function handler(req, res) {
   res.status(200).json(data, cityData);
   }
 
-  // cron.schedule('* 59 * * *', async () => {
-  //   try {
-  //     await fetchWeatherData();
-  //   } catch (error) {
-  //     console.error('Erreur lors du chargement des données', error);
-  //   }
-  // });
   
